3258241a347cd45a4ce056c2251c3a9024d98836
Your Name <you@example.com>
2020-09-23 11:35:10 +0300

    user_account

--- user.py
+++ user.py
@@ -8,6 +8,13 @@ user2 = {'name': 'Петр', 'age': 25, 'account': account2}
 user3 = {'name': 'Ольга', 'age': 18, 'account': account3}
 user4 = {'name': 'Анна', 'age': 27, 'account': account4}
 
+User_list = [user1, user2, user3, user4]
+
+user1 = user[0]
+user2 = user[1]
+user3 = user[2]
+user4 = user[3]
+
 key = input('Введите ключ (name или account): ')
 if key == 'NaMe':
     print(f"значение ключа name для юзера 1: {user1['name']}")
@@ -19,12 +26,10 @@ else:
 
 number = input('Введите порядковый номер: ')
 
-User_list = [user1, user2, user3, user4]
-
-user1 = user[0]
-user2 = user[1]
-user3 = user[2]
-user4 = user[3]
+user1 = {'name': 'Иван', 'age': 20, 'account': account1}
+user2 = {'name': 'Петр', 'age': 25, 'account': account2}
+user3 = {'name': 'Ольга', 'age': 18, 'account': account3}
+user4 = {'name': 'Анна', 'age': 27, 'account': account4}
 
 if number == 1:
     print(f"имя: user0['name']")

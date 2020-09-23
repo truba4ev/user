186506fa27b0d5b5dcebe93c1eb619dbaeeb0d99
Your Name <you@example.com>
2020-09-24 00:09:32 +0300

    user_account

--- user.py
+++ user.py
@@ -10,10 +10,10 @@ user4 = {'name': 'Анна', 'age': 27, 'account': account4}
 
 User_list = [user1, user2, user3, user4]
 
-user1 = user[0]
-user2 = user[1]
-user3 = user[2]
-user4 = user[3]
+#user1 = user[0]
+#user2 = user[1]
+#user3 = user[2]
+#user4 = user[3]
 
 key = input('Введите ключ (name или account): ')
 if key == 'NaMe':
@@ -24,16 +24,31 @@ if key == 'NaMe':
 else:
     print('Введенный ключ не найден')
 
-number = input('Введите порядковый номер: ')
-
-user1 = {'name': 'Иван', 'age': 20, 'account': account1}
-user2 = {'name': 'Петр', 'age': 25, 'account': account2}
-user3 = {'name': 'Ольга', 'age': 18, 'account': account3}
-user4 = {'name': 'Анна', 'age': 27, 'account': account4}
+number = int(input('Введите порядковый номер: '))
 
 if number == 1:
-    print(f"имя: user0['name']")
-    print(f"возраст: user0['age']")
-    print(f"Логин: user0['login']")
+    print(f"имя: {user1['name']}")
+    print(f"возраст: {user1['age']}")
+    print(f"Логин: {account1['login']}")
+    print(f"Пароль: {account1['password']}")
 else:
+    number == 2
+    print(f"имя: {user2['name']}")
+    print(f"возраст: {user2['age']}")
+    print(f"Логин: {account2['login']}")
+    print(f"Пароль: {account2['password']}")
+elif:
+    number == 3
+    print(f"имя: {user3['name']}")
+    print(f"возраст: {user3['age']}")
+    print(f"Логин: {account3['login']}")
+    print(f"Пароль: {account3['password']}")
+elif:
+    number == 4
+    print(f"имя: {user4['name']}")
+    print(f"возраст: {user4['age']}")
+    print(f"Логин: {account4['login']}")
+    print(f"Пароль: {account4['password']}")
+else:
+    number > 4
     print('Нет такой карточки')
